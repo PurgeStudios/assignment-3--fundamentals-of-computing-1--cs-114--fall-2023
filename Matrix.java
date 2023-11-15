1fa0d50c09d205b03a04e3acc488ca86641964b9
Dimitri <56205878+PurgeStudios@users.noreply.github.com>
2023-11-15 11:08:11 -0500

    Started Matrix + RunMatrix
    Added constructor that reads user input
    Added printMatrix

--- Matrix.java
+++ Matrix.java
@@ -1,2 +1,34 @@
 public class Matrix {
+
+  public static int[][] matrix;
+
+  public static void matrix(int size) { // Constructor that accepts User's Input as size
+
+    matrix = new int[size][size];
+
+    System.out.println("Your matrix is " + size + " x " + size);
+  }
+
+  public static void printMatrix(int size) {
+
+    for (int row = 0; row < matrix.length; row++) {
+      for (int colomn = 0; colomn < matrix.length; colomn++)
+
+        if (row + colomn == matrix.length - 1) { //Highlighs every diagonal for this truth
+          System.out.print("\u001B[33m" + matrix[row][colomn] + "\u001B[0m \t");
+        }
+        else {
+          System.out.print(matrix[row][colomn] + "\t");
+        }
+      System.out.println();
+    }
+  }
+  public static void populateMatrix() {
+
+  }
+
+
+
+  //   private void swap(int x1, int y1, int x2, int y2) {
+  // }
 }
--- RunMatrix.java
+++ RunMatrix.java
@@ -2,5 +2,20 @@ import java.util.Scanner;
 
 public class RunMatrix {
   public static void main(String[] args) {
+
+    System.out.print("Please enter the size of your matrix: ");
+    Scanner scan = new Scanner(System.in);
+
+    int userInput = scan.nextInt();
+    System.out.println(); //Spaces output for visibility
+
+    Matrix.matrix(userInput);
+    System.out.println();
+
+    Matrix.printMatrix(userInput);
+    System.out.println();
+
+    scan.close();
+
+    }
   }
-}
